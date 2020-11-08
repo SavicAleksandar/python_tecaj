72872ca3ccec66fae31ab7d59deafaa257b5ef25
SavicAleksandar <62108855+SavicAleksandar@users.noreply.github.com>
2020-11-08 22:28:56 +0100

    Update 5_5_domaca_naloga.py

--- 5_5_domaca_naloga.py
+++ 5_5_domaca_naloga.py
@@ -4,34 +4,35 @@ import random
 
 def get_score_list():
     with open("score.txt", "r") as score_file:
-        score_list = json.load(score_file.read())
-        return(score_list)
+        score_list = json.load(score_file)
+    return(score_list)
 
 def top_scores():
-    return (get_score_list())
-    print("Top scores: " + str(get_score_list()))
-    for score_dict in get_score_list():
-        return(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+    score_list = get_score_list()
+    result_top3 = sorted(score_list, key=lambda k: k["attempts"])[:3]
+    return ("Top 3 scores: ", result_top3)
 
 def play_game():
+    attempts = 0
     secret = random.randint(1, 30)
+    name = input("What is your name: ")
     level = input("Enter your level 'H' - hard or 'E' - easy: ")
     def play_game_level(level):
         if level == "H":
-            return ("Wrong guess. Try again!")
+            print ("Wrong guess. Try again!")
         elif level == "E" and guess > secret:
-            return ("Try something smaller.")
+            print ("Try something smaller.")
         else:
-            return ("Try something bigger.")
+            print ("Try something bigger.")
 
     while True:
-        attempts = 0
+        attempts += 1
         guess = int(input("Guess the secret number (between 1 and 30): "))
         if guess == secret:
             score_list = get_score_list()
-            attempts += 1
-            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
-            with open("score_list.txt", "w") as score_file:
+
+            score_list.append({"attempts": attempts, "name": name, "date": str(datetime.datetime.now())})
+            with open("score.txt", "w") as score_file:
                 score_file.write(json.dumps(score_list))
             print("You've guessed it - congratulations! It's number " + str(secret))
             print("Attempts needed: " + str(attempts))
@@ -42,8 +43,6 @@ def play_game():
         elif guess < secret:
             print(play_game_level(level))
 #            print("Your guess is not correct... try something bigger")
-
-attempts = 0
 
 while True:
     selection = input("Would you like to A - play a game, B - see the best scores or C - quit?")
